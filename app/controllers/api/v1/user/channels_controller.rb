370937eb255e3ea56024bf24eb48843bebcc6663
Nguyen The Thang <justin.ntt@gmail.com>
2016-09-26 17:42:36 +0700

    model

--- app/controllers/api/v1/user/channels_controller.rb
+++ app/controllers/api/v1/user/channels_controller.rb
@@ -10,9 +10,10 @@ class Api::V1::User::ChannelsController < Api::V1::User::BaseController
   end
 
   def create
-    service = Chat::ChannelService.new create_params[:user_ids], create_params[:twilio_channel_sid]
-    if service.call
-      render json: { success: true }, status: 201
+    service = Chat::ChannelService.new create_params[:user_ids], create_params
+    channel = service.call
+    if channel
+      render json: { success: true, data: channel }, status: 201
     else
       render json: { success: false, error: service.errors.full_messages.last }, status: 422
     end
@@ -21,6 +22,6 @@ class Api::V1::User::ChannelsController < Api::V1::User::BaseController
   private
 
   def create_params
-    params.require(:channel).permit(:twilio_channel_sid, user_ids: [])
+    params.require(:channel).permit(:twilio_channel_sid, :friendly_name, user_ids: [])
   end
 end
