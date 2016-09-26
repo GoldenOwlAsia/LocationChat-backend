37c526f818d49c2ebb409d4d932e9e56836ce621
Nguyen The Thang <justin.ntt@gmail.com>
2016-09-26 12:24:29 +0700

    implement services

---
+++ app/services/twilio_auth_service.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+class TwilioAuthService < BaseService
+
+  validates :device_token, presence: true
+  validates :identity_name, presence: true
+
+  attr_reader :device_token, :identity_name
+
+  def initialize(user_id, device_token)
+    @device_token = device_token
+    @identity_name = user_id
+  end
+
+  def call
+    validate
+    token.add_grant grant
+    token.to_jwt
+  end
+
+  private
+
+  def end_point
+    "#{Rails.application.secrets.twilio_app_name}:#{@identity_name}:#{@device_token}"
+  end
+
+  def token
+    Twilio::Util::AccessToken.new Rails.application.secrets.twilio_account_sid,
+      Rails.application.secrets.twilio_api_key, Rails.application.secrets.twilio_api_secret, 3600, @identity_name
+  end
+
+  def grant
+    grant = Twilio::Util::AccessToken::IpMessagingGrant.new
+    grant.service_sid = Rails.application.secrets.twilio_service_sid
+    grant.endpoint_id = end_point
+    grant
+  end
+
+  def token_validity
+    errors.add(:identity_name, :invalid) unless @device_token.identity_name && ActiveSupport::SecurityUtils.secure_compare(@device_token.identity_name, @identity_name)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
+
       namespace :admin do
         resources :users, only: [:index, :show, :create, :update] do
           member do
@@ -26,6 +27,7 @@ Rails.application.routes.draw do
             delete :destroy
           end
         end
+        resources :tokens, only: [:create]
       end
     end
   end
