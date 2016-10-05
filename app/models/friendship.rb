13d5312f4ce0e5b8065a7d907e65ba5e44b346e0
Nguyen The Thang <justin.ntt@gmail.com>
2016-10-05 15:51:03 +0700

    code climate

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -16,5 +16,8 @@ class Friendship < ActiveRecord::Base
 
   validates_uniqueness_of :to_user_id, scope: [:from_user_id], message: 'already existed'
 
-  scope :in_friendship, ->(user_id, another_user_id) { where('(from_user_id = :user_id AND to_user_id = :another_user_id ) OR (from_user_id = :another_user_id AND to_user_id = :user_id)', user_id: user_id, another_user_id: another_user_id) }
+  scope :in_friendship, ->(user_id, another_user_id) {
+                              where('(from_user_id = :user_id AND to_user_id = :another_user_id ) OR
+                                    (from_user_id = :another_user_id AND to_user_id = :user_id)',
+                                    user_id: user_id, another_user_id: another_user_id) }
 end
