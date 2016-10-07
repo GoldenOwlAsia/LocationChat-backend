52f761d8ec8e98505b8fbe4e32fb162cc8415ddd
Nguyen The Thang <justin.ntt@gmail.com>
2016-10-07 10:11:17 +0700

    update places

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -26,8 +26,12 @@ RSpec.describe Place, type: :model do
 
     context 'within range' do
       it "returns places" do
-        place = create :place
-        result = Place.within_radius(place.latitude, place.longitude).to_a
+        latitude = 10
+        longitude = 100
+        place = create :place, latitude: latitude, longitude: longitude
+        puts place.longitude
+        puts place.latitude
+        result = Place.within_radius(latitude + 0.0000000000001, longitude + 0.0000000000001).to_a
         expect(result).to eq [place]
       end
     end
