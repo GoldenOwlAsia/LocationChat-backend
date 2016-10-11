944a9a760a21ed8ed32d85a0c91b90b1bb60a1e4
Federer <hoangtrunghieu576@gmail.com>
2016-10-11 15:16:35 +0700

    update test case

--- spec/services/friend_request_service_spec.rb
+++ spec/services/friend_request_service_spec.rb
@@ -3,27 +3,50 @@ require 'rails_helper'
 RSpec.describe FriendRequestService do
   let(:user) { create :user }
   let(:another_user) { create :user }
-  it "is valid" do
-    service = FriendRequestService.new(user.id, another_user.id)
-    expect(service.valid?).to eq true
+
+  subject { FriendRequestService.new(user.id, another_user.id) }
+  describe 'Check subject is valid?' do
+    it "is valid" do
+      expect(subject.valid?).to eq true
+    end
   end
 
-  it "creates 2 records in friend_requests" do
-    expect {
-      FriendRequestService.new(user.id, another_user.id).send_request
-    }.to change{FriendRequest.count}.by 2
+  describe 'POST #send_add_friend' do
+    it "creates 2 records in friend_requests" do
+      expect {
+        subject.send_request
+      }.to change{FriendRequest.count}.by 2
+    end
+
+    it "can't create 2 records when invalid params" do
+      subject.to_user_id = nil
+      expect(subject.valid?).to eq false
+    end
   end
 
-  it "create 2 records in friendship when accept friend" do
-    expect {
-      FriendRequestService.new(user.id, another_user.id).accept_request
-    }.to change{Friendship.count}.by 2
+  describe 'POST #accept_add_friend' do
+    it "create 2 records in friendship when accept friend" do
+      expect {
+        subject.accept_request
+      }.to change{Friendship.count}.by 2
+    end
+
+    it "can't create 2 records to friendship when invalid params" do
+      subject.to_user_id = nil
+      expect(subject.valid?).to eq false
+    end
   end
 
-  it "remove 2 record in friendship when declined friend" do
-    service = FriendRequestService.new(user.id, another_user.id).reject_request
-    expect {
-      service
-    }.to change{Friendship.count}.by 0
+  describe 'POST #reject_add_friend' do
+    it "remove 2 record in friendship when declined friend" do
+      expect {
+        subject.reject_request
+      }.to change{Friendship.count}.by 0
+    end
+
+    it "can't delete 2 records in friendship when invalid params" do
+      subject.to_user_id = nil
+      expect(subject.valid?).to eq false
+    end
   end
 end
