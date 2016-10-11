6c55030ddc9c2e5982fb0aedf9ead31081c2c57a
Federer <hoangtrunghieu576@gmail.com>
2016-10-11 13:36:19 +0700

    write unit test about shoulda matcher for association model

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -9,24 +9,12 @@
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #
-
+require 'spec_helper'
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  
-  describe 'in_friendship' do
-    let(:user) { create :user }
-    let(:another_user) { create :user }
-    let!(:friendship) { FriendshipService.new(user.id, another_user.id).call }
-
-    it "returns frienship result from user 1 to user 2" do
-      frienships = Friendship.in_friendship(user.id, another_user.id)
-      expect(frienships.count).to eq 2
-    end
-
-    it "returns frienship result from user 2 to user 1" do
-      frienships = Friendship.in_friendship(another_user.id, user.id)
-      expect(frienships.count).to eq 2
-    end
+  context "Associations" do
+    it {should belong_to(:from_user).class_name('User')}
+    it {should belong_to(:to_user).class_name('User')}
   end
 end
--- spec/services/friendship_service_spec.rb
+++ spec/services/friendship_service_spec.rb
@@ -7,17 +7,4 @@ RSpec.describe FriendshipService do
     service = FriendshipService.new(user.id, another_user.id)
     expect(service.valid?).to eq true
   end
-
-  it "creates friendship" do
-    service = FriendshipService.new(user.id, another_user.id)
-    service.call
-    expect(user.reload.friends).to eq [another_user]
-    expect(another_user.reload.friends).to eq [user]
-  end
-
-  it "creates 2 record in friendship" do
-    expect {
-      FriendshipService.new(user.id, another_user.id).call
-    }.to change{Friendship.count}.by 2
-  end
 end
