e73cab8e770bf1b472c09a4d76226eaafa902b7e
Federer <hoangtrunghieu576@gmail.com>
2016-10-11 16:30:52 +0700

    refactor code

--- app/models/concerns/publish.rb
+++ app/models/concerns/publish.rb
@@ -6,7 +6,6 @@ module Publish
 
     validates_uniqueness_of :to_user_id, scope: [:from_user_id], message: 'already existed'
 
-    scope :in_friendship, ->(user_id, another_user_id) {
-                              where('(from_user_id = :user_id AND to_user_id = :another_user_id ) OR (from_user_id = :another_user_id AND to_user_id = :user_id)', user_id: user_id, another_user_id: another_user_id) }
+    scope :in_friendship, ->(user_id, another_user_id) {where('(from_user_id = :user_id AND to_user_id = :another_user_id ) OR (from_user_id = :another_user_id AND to_user_id = :user_id)', user_id: user_id, another_user_id: another_user_id)}
   end
 end
