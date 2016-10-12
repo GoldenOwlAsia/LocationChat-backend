4af631fbcb56c069592620c262ecc2a160e293b8
Federer <hoangtrunghieu576@gmail.com>
2016-10-12 15:36:53 +0700

    remove comment

--- config/initializers/apns.rb
+++ config/initializers/apns.rb
@@ -1,8 +1,5 @@
 APNS.host = 'gateway.push.apple.com'
-# gateway.sandbox.push.apple.com is default
 
 APNS.pem = File.join(Rails.root, "Certificates.pem")
-# this is the file you just created
 
 APNS.port = 2195
-# this is also the default. Shouldn't ever have to set this, but just in case Apple goes crazy, you can.
