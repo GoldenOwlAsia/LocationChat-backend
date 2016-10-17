b3cfd61173694213f283cc7113ac28b92c9d1214
Federer <hoangtrunghieu576@gmail.com>
2016-10-17 13:29:59 +0700

    update last sign in at

--- app/controllers/api/v1/user/base_controller.rb
+++ app/controllers/api/v1/user/base_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::User::BaseController < Api::V1::BaseController
-  before_action :authenticate_user_from_token!
+  before_action :authenticate_user_from_token!, :update_last_sign_in_at
 
   def current_user
     @user
@@ -9,6 +9,13 @@ class Api::V1::User::BaseController < Api::V1::BaseController
 
   def authenticate_user_from_token!
     authenticate_from_token!(User)
+  end
+
+  def update_last_sign_in_at
+    if user_signed_in? && !session[:logged_signin]
+      sign_in(current_user, force: true)
+      session[:logged_signin] = true
+    end
   end
 
   def find_user_from_token
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  before_action :update_last_sign_in_at
-
-  private
-
-  def update_last_sign_in_at
-    if user_signed_in? && !session[:logged_signin]
-      sign_in(current_user, force: true)
-      session[:logged_signin] = true
-    end
-  end
 end
