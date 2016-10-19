4dffe5555ad3f41ddf0e83ac9103985ed1ba912f
Nguyen The Thang <justin.ntt@gmail.com>
2016-10-19 18:23:42 +0700

    update service

--- app/services/twilio_auth_service.rb
+++ app/services/twilio_auth_service.rb
@@ -18,9 +18,22 @@ class TwilioAuthService < BaseService
   private
 
   def update_service_settings
-    ip_messaging_client = Twilio::REST::IpMessagingClient.new(Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token)
-    service = ip_messaging_client.services.get Rails.application.secrets.twilio_service_sid
-    service&.update readStatusEnabled: true, reachabilityEnabled: true
+    # curl -X POST https://ip-messaging.twilio.com/v1/Services/ISe4aaa88ede714ca0b80a1507742b60a5 -d 'Notifications.NewMessage.Enabled=true' -d 'Notifications.NewMessage.Template=A New message in ${CHANNEL} from ${USER}: ${MESSAGE}' -u 'ACe645aed6cefa9bf1e08932bb9d7e93ba:6d6efec0e6878bf682d055f880319de4'
+    uri = URI.parse("https://ip-messaging.twilio.com/v1/Services/#{Rails.application.secrets.twilio_service_sid}")
+    request = Net::HTTP::Post.new(uri)
+    request.basic_auth(Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token)
+    request.body = "ReadStatusEnabled=true&ReachabilityEnabled=true&Notifications.NewMessage.Enabled=true&Notifications.NewMessage.Template=A New message in ${CHANNEL} from ${USER}: ${MESSAGE}"
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+      http.request(request)
+    end
+
+    puts "successul service udpate"
+    puts response.body
+
+    # ip_messaging_client = Twilio::REST::IpMessagingClient.new(Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token)
+    # service = ip_messaging_client.services.get Rails.application.secrets.twilio_service_sid
+    # service&.update read_status_enabled: true, reachability_enabled: true, notifications: { enabled: true }
+    # binding.pry
   end
 
   def end_point
