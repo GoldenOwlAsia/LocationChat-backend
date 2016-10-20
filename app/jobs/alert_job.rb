2be5364d58d85341351e85cebccd426c45596885
Nguyen The Thang <justin.ntt@gmail.com>
2016-10-21 01:57:41 +0700

    improve logic

--- app/jobs/alert_job.rb
+++ app/jobs/alert_job.rb
@@ -3,6 +3,8 @@ class AlertJob < ApplicationJob
     @from_user = User.find data[0]
     @to_user = User.find data[1]
     @alert_msg = "#{@from_user.name}: #{data[2]}"
-    APNS.send_notification(@to_user.device_token, alert: @alert_msg, other: { user_name: @from_user.name, url: @from_user.url_image_picture, id: @from_user.id, msg: data[2], sid: data[3] })
+    if @to_user.device_token.present?
+      APNS.send_notification(@to_user.device_token, alert: @alert_msg, other: { from_user_id: @from_user.id, msg: data[2], sid: data[3] })
+    end
   end
 end
