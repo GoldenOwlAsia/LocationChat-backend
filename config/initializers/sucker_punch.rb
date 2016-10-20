28efaf1f02ec09ef2929a1c8427a0313b7954619
Nguyen The Thang <justin.ntt@gmail.com>
2016-10-20 23:08:35 +0700

    update places job

---
+++ config/initializers/sucker_punch.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby
+# @Author: Nguyen The Thang
+# @Date:   2016-10-20 22:54:41
+# @Last Modified by:   Nguyen The Thang
+# @Last Modified time: 2016-10-20 22:54:44
+require 'sucker_punch/async_syntax'
