0eb2d8b0723078d7e7c6a2268532f6e249b76f85
tranquangvu <vutran.513@gmail.com>
2016-11-10 16:44:34 +0700

    Add sound, name, url to APNS send_notification

--- app/jobs/alert_job.rb
+++ app/jobs/alert_job.rb
@@ -3,8 +3,19 @@ class AlertJob < ApplicationJob
     @from_user = User.find data[0]
     @to_user = User.find data[1]
     @alert_msg = "#{@from_user.name}: #{data[2]}"
+
     if @to_user.device_token.present?
-      APNS.send_notification(@to_user.device_token, alert: @alert_msg, other: { from_user_id: @from_user.id, msg: data[2], sid: data[3] })
+      APNS.send_notification(
+        @to_user.device_token,
+        alert: @alert_msg,
+        sound: 'default',
+        other: { 
+          from_user_id: @from_user.id,
+          name: @from_user.name,
+          url: @from_user.url_image_picture,
+          sid: data[3]
+        }
+      )
     end
   end
 end
