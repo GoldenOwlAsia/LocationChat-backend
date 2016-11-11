84d2e5dfbed5d920af6152ff573d9221f5491762
tranquangvu <vutran.513@gmail.com>
2016-11-11 13:53:38 +0700

    update profile controller

--- app/controllers/api/v1/user/profiles_controller.rb
+++ app/controllers/api/v1/user/profiles_controller.rb
@@ -1,29 +1,26 @@
 class Api::V1::User::ProfilesController < Api::V1::User::BaseController
   skip_before_action :authenticate_user_from_token!, only: [:check, :create]
-  before_action :find_user, only: [:show]
+  before_action :find_target_user, only: [:show, :update]
+  
   def index
   end
 
   def show
-    @c_user.friend_requests.map(&:to_user).each do |to_user|
-      @status = to_user.friend_requests.map(&:status).first if to_user.id == @user.id
+    current_user.friend_requests.map(&:to_user).each do |to_user|
+      @status = to_user.friend_requests.map(&:status).first if to_user.id == @target_user.id
     end
-    render json: { success: true, data: UserSerializer.new(@user), status: @status }
+    render json: { success: true, data: UserSerializer.new(@target_user), status: @status }
   end
 
   def check
-    @user = User.find_by provider: check_params[:provider], uid: check_params[:uid]
-    if @user
-      render json: { success: true }
-    else
-      render json: { success: false }
-    end
+    @user = User.find_by(provider: check_params[:provider], uid: check_params[:uid])
+    render json: { success: @user ? true : false }
   end
 
   def create
-    @user = User.new create_profile_params.except(:photos)
-    create_profile_params[:photos].split(',').map(&:strip).each do |photo|
-      @user.photos.build url: photo
+    @user = User.new(create_profile_params.except(:photos))
+    create_profile_params[:photos].split(',').each do |photo|
+      @user.photos.build(url: photo.strip)
     end if create_profile_params[:photos].present?
     if @user.save
       render json: { success: true, data: UserSerializer.new(@user.reload) }, status: 201
@@ -33,24 +30,27 @@ class Api::V1::User::ProfilesController < Api::V1::User::BaseController
   end
 
   def update
-    @user = current_user
-    @user.attributes = update_profile_params.except :photos
-    current_user.list_photos
-    update_profile_params[:photos].split(',').map(&:strip).each do |photo|
-      @user.photos.build url: photo
-    end if update_profile_params[:photos].present?
-    if @user.save
-      render json: { success: true, data: UserSerializer.new(@user.reload) }, status: 200
+    if current_user != @target_user
+      render json: { success: false, error: "This profile isn't belongs to you" }, status: 200 and return
+    end
+    current_user.attributes = update_profile_params.except(:photos)
+    if update_profile_params[:photos].present?
+      current_user.destroy_photos
+      update_profile_params[:photos].split(',').each do |photo|
+        current_user.photos.build(url: photo.strip)
+      end
+    end
+    if current_user.save
+      render json: { success: true, data: UserSerializer.new(current_user) }, status: 200
     else
-      render json: { success: false, error: @user.errors.full_messages.last }, status: 422
+      render json: { success: false, error: current_user.errors.full_messages.last }, status: 422
     end
   end
 
   private
 
-  def find_user
-    @c_user = current_user
-    @user = User.find params[:id]
+  def find_target_user
+    @target_user = User.find(params[:id])
   end
 
   def check_params
@@ -58,10 +58,12 @@ class Api::V1::User::ProfilesController < Api::V1::User::BaseController
   end
 
   def create_profile_params
-    params.require(:profile).permit(:provider, :uid, :device_token, :first_name, :last_name, :number_phone, :email, :url_image_picture, :phone_country_code, :home_city, :location, :latitude, :longitude, :photos)
+    params.require(:profile).permit(:provider, :uid, :device_token, :first_name, :last_name, :number_phone,
+      :email, :url_image_picture, :phone_country_code, :home_city, :location, :latitude, :longitude, :photos)
   end
 
   def update_profile_params
-    params.require(:profile).permit(:first_name, :last_name, :device_token, :number_phone, :email, :url_image_picture, :phone_country_code, :home_city, :location, :latitude, :longitude, :photos)
+    params.require(:profile).permit(:first_name, :last_name, :device_token, :number_phone, :email,
+      :url_image_picture, :phone_country_code, :home_city, :location, :latitude, :longitude, :photos)
   end
 end
